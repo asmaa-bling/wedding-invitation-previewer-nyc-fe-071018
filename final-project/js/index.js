cf93415efdb53876836f5a7a4287b8e4a03be531
Asmaa Abbas <asmaa@flatironschool.com>
2018-09-06 23:27:33 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -4,11 +4,11 @@ document.getElementById("lastName").addEventListener("keydown", (event) => {
   let currKey = event.key;
   if(event.keyCode === 9 || event.keyCode === 13 || event.keyCode === 16){
     return false;
+  } else if (event.keyCode === 8){
+    currVal = currVal.slice(0, -1);
+  } else {
+    currVal += currKey;
   }
   
-  if (event.keyCode === 8){
-    currVal += currVal.slice(0, -1);
-  }
-  currVal += currKey; 
   document.getElementById("displayLastName").innerHTML = currVal;
 });
