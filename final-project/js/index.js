a5012698c5d02d9f921908992f2a37fe6321ba7d
Asmaa Abbas <asmaa@flatironschool.com>
2018-09-06 23:03:23 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -1,4 +1,5 @@
 document.getElementById("lastName").addEventListener("keydown", (event) => {
-  const keyVal = event.key;
-  document.getElementById("displayLastName").innerHTML = keyVal;
+  const eventKey = event.key;
+  let fullInput = 
+  document.getElementById("displayLastName").innerHTML = eventKey;
 });
