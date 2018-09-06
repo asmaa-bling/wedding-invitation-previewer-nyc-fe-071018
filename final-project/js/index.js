2fc68b3eb71fb374bd143d507632f7e993e20deb
Asmaa Abbas <asmaa@flatironschool.com>
2018-09-06 23:19:30 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -2,7 +2,7 @@ let currVal = "";
 
 document.getElementById("lastName").addEventListener("keydown", (event) => {
   let currKey = event.key;
-  if(event.keyCode === 8){
+  if(event.keyCode === 8 ){
     return false;
   }
   currVal += currKey; 
