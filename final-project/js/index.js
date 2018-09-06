4323a313f39c8929eb17c6241dbac765eb54e98a
Asmaa Abbas <asmaa@flatironschool.com>
2018-09-06 23:17:29 +0000

    automatically backed up by learn

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -2,6 +2,7 @@ let currVal = "";
 
 document.getElementById("lastName").addEventListener("keydown", (event) => {
   let currKey = event.key;
+  if(currkey)
   currVal += currKey; 
   document.getElementById("displayLastName").innerHTML = currVal;
 });
